be98680e4d58c006761ba04ff7ed4ddcec69cac3
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-23 12:55:18 +0100

    Delete custom_components/hwam directory

--- custom_components/hwam/__init__.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the HWAM integration."""
-    hass.data[DOMAIN] = {}
-    return True
