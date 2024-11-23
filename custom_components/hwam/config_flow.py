6550a3907e51f671c889d6d4d55da59cb40568a4
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-23 11:22:25 +0100

    Update config_flow.py

--- custom_components/hwam/config_flow.py
+++ custom_components/hwam/config_flow.py
@@ -1,7 +1,17 @@
 from homeassistant import config_entries
-from .const import DOMAIN
+from homeassistant.core import callback
+from .const import DOMAIN, CONF_IP_ADDRESS
 
+@config_entries.HANDLERS.register(DOMAIN)
 class HWAMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """HWAM configuration flow."""
+
     async def async_step_user(self, user_input=None):
-        # Configuration flow will be implemented here
-        pass
+        """Handle the initial step."""
+        if user_input is not None:
+            return self.async_create_entry(title="HWAM Stove", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_IP_ADDRESS): str}),
+        )
