01a6ccfe1211d70d85e09a59b240364107fe0516
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-23 11:15:50 +0100

    Create const.py

---
+++ custom_components/hwam/const.py
@@ -0,0 +1,14 @@
+DOMAIN = "hwam"
+DEFAULT_NAME = "HWAM Stove"
+CONF_IP_ADDRESS = "ip_address"
+
+# API keys from the HWAM stove
+API_KEYS = [
+    "stove_temperature",
+    "room_temperature",
+    "oxygen_level",
+    "valve1_position",
+    "valve2_position",
+    "valve3_position",
+    "door_open"
+]
