84a2f2a1a1ac0bf69a8b0393bc6e6de987bc4c87
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-23 11:21:28 +0100

    Update __init__.py

--- custom_components/hwam/__init__.py
+++ custom_components/hwam/__init__.py
@@ -1,1 +1,8 @@
-# Initialize HWAM integration
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the HWAM integration."""
+    hass.data[DOMAIN] = {}
+    return True
